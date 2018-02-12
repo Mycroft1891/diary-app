4fe5b41c8f69d829d3bd4bf3af6459f5afc264d8
Mycroft1891 <mycroft1891@gmail.com>
2018-02-12 19:09:02 +0100

    fix trix editor image bug

--- app/assets/javascripts/trix_upload.js
+++ app/assets/javascripts/trix_upload.js
@@ -27,8 +27,7 @@ function uploadAttachment(attachment) {
       var data = JSON.parse(xhr.responseText);
       return attachment.setAttributes({
         url: data.image_url,
-        href: data.image_url,
-        width: '100%',
+        href: data.image_url
       })
     }
   }
@@ -39,6 +38,28 @@ function uploadAttachment(attachment) {
 document.addEventListener('trix-attachment-add', function(e) {
   var attachment = e.attachment;
   if(attachment.file) {
-    return uploadAttachment(attachment);
+    uploadAttachment(attachment);
+  }
+});
+
+document.addEventListener('trix-attachment-remove', function(e) {
+  var attachment = e.attachment;
+  console.log(attachment);
+});
+
+document.addEventListener('trix-initialize', function(e) {
+  console.log('init trix editor');
+  var buttonHTML, fileInputHTML;
+  buttonHTML = "<button type=\"button\" class=\"attach\" data-trix-action=\"x-attach\">Attach Files</button>";
+  fileInputHTML = "<input type=\"file\" multiple>";
+  $(Trix.config.toolbar.content).find(".button_group.block_tools").append(buttonHTML);
+});
+
+document.addEventListener('turbolinks:load', function (e) {
+  console.log('editor change detected');
+  var images = document.getElementsByTagName('img')
+  for (img of images) {
+    img.removeAttribute('height');
+    img.removeAttribute('width');
   }
 });
